a0d140d18692b4a2277564fcdce11298970fe124
Louis Poirier <contact@louispoirier.io>
2022-12-03 19:15:48 +0100

    feat: add declarations of Dart typed errors to mirror web API errors.

--- lib/src/api/errors.dart
+++ lib/src/api/errors.dart
@@ -1,3 +1,19 @@
+/// Thrown if a user dismisses the prompt without making a selection or if a file selected is deemed too sensitive or
+/// dangerous to be exposed to the website.
 class AbortError {}
 
+/// Thrown if you try to access a file while [PermissionState] is not [granted] by user.
 class NotAllowedError {}
+
+/// Thrown if the name specified is not a valid string or contains characters not allowed on the file system.
+class MalformedNameError {}
+
+/// Thrown if the named entry is a directory and not a file when using [FileSystemDirectoryHandle.getFileHandle].
+/// Throw if the named entry is a file and not a directory when using [FileSystemDirectoryHandle.getDirectoryHandle].
+class TypeMismatchError {}
+
+/// Thrown if [recursive] is set to [false] and the entry to be removed has children.
+class InvalidModificationError {}
+
+/// Thrown if file doesn't exist and the create option is set to [false].
+class NotFoundError {}
